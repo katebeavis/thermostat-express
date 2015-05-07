85fd3890868682a20d7f969ec3f8917e90d50ad5
katebeavis <katebeavis84@gmail.com>
2015-05-08 00:30:00 +0100

    Changed port on server for heroku

--- server.js
+++ server.js
@@ -8,6 +8,6 @@ app.set('view engine', 'ejs');
 app.use('/public', express.static(__dirname + '/public'));
 app.engine('html', require('ejs').renderFile);
 
-var server=app.listen(3000,function(){
+var server=app.listen(process.env.PORT || 3000,function(){
 console.log("Express is running on port 3000");
 });
